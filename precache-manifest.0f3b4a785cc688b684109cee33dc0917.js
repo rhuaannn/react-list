3051aa63c09f430a0283592f75dc77ec9e90658f
rhuaannn <rhuanmarques1223@gmail.com>
2022-08-22 22:12:14 -0300

    Updates

---
+++ precache-manifest.0f3b4a785cc688b684109cee33dc0917.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cdcc6d5c5dd1f2e3393",
+    "url": "/react-list.git/static/css/main.07c63e86.chunk.css"
+  },
+  {
+    "revision": "4cdcc6d5c5dd1f2e3393",
+    "url": "/react-list.git/static/js/main.4cdcc6d5.chunk.js"
+  },
+  {
+    "revision": "2dcb266682758070661d",
+    "url": "/react-list.git/static/js/1.2dcb2666.chunk.js"
+  },
+  {
+    "revision": "82b6ecd8203148b9342c",
+    "url": "/react-list.git/static/js/runtime~main.82b6ecd8.js"
+  },
+  {
+    "revision": "e077c991ebfe2d77651107091f511a40",
+    "url": "/react-list.git/index.html"
+  }
+];
